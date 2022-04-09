5b199727fdb926c736248bbde8acf50fa9aca516
Gradio2000 <laskin999@gmail.com>
2022-04-09 20:50:55 +0500

    Initial commit

---
+++ src/main/java/com/example/system5/System5Application.java
@@ -0,0 +1,13 @@
+package com.example.system5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class System5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(System5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/system5/System5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.system5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class System5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
