44dcad718d5ff68ce6fa9a15143609826ff4d702
Gradio2000 <laskin999@gmail.com>
2022-04-10 20:42:27 +0500

    Настройка swagger

--- src/main/java/com/example/system5/config/OpenApiConfig.java
+++ src/main/java/com/example/system5/config/OpenApiConfig.java
@@ -5,7 +5,11 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+
 
 @Configuration
 //https://sabljakovich.medium.com/adding-basic-auth-authorization-option-to-openapi-swagger-documentation-java-spring-95abbede27e9
@@ -24,5 +28,13 @@ import org.springframework.context.annotation.Configuration;
 )
 public class OpenApiConfig {
 
+        @Bean
+        public GroupedOpenApi api() {
+                return GroupedOpenApi.builder()
+                        .group("REST API")
+                        .pathsToMatch("/**")
+                        .pathsToExclude("/profile/**")
+                        .build();
+        }
 
 }
