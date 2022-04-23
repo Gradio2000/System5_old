42d43cf16a2ee0a49d1f50fdf47fb008683dad83
Gradio2000 <laskin999@gmail.com>
2022-04-23 10:10:40 +0500

    заполнение БД

--- src/main/resources/DB/5_populUserAndRole.sql
+++ src/main/resources/DB/5_populUserAndRole.sql
@@ -28,8 +28,14 @@ create index idx_user_name
 
 INSERT INTO USERS (login, NAME, PASSWORD, position_id)
 VALUES ('aa', 'Ласкин', '$2a$12$WIr/8OS3JN02CakLS9RiyuQuWm8Fk7XybV7WyVzidNfKoZJ.hxFSi', 1),
-       ('bb', 'Даянов', '$2a$12$WIr/8OS3JN02CakLS9RiyuQuWm8Fk7XybV7WyVzidNfKoZJ.hxFSi', 3),
-       ('cc', 'admin', '$2a$12$WIr/8OS3JN02CakLS9RiyuQuWm8Fk7XybV7WyVzidNfKoZJ.hxFSi', 9)
+       ('bb', 'Даянов', '$2a$12$WIr/8OS3JN02CakLS9RiyuQuWm8Fk7XybV7WyVzidNfKoZJ.hxFSi', 2),
+       ('cc', 'Степанов', '$2a$12$WIr/8OS3JN02CakLS9RiyuQuWm8Fk7XybV7WyVzidNfKoZJ.hxFSi', 3),
+       ('dd', 'Першина', '$2a$12$WIr/8OS3JN02CakLS9RiyuQuWm8Fk7XybV7WyVzidNfKoZJ.hxFSi', 4),
+       ('ee', 'Лапшина', '$2a$12$WIr/8OS3JN02CakLS9RiyuQuWm8Fk7XybV7WyVzidNfKoZJ.hxFSi', 5),
+       ('ff', 'Сытник', '$2a$12$WIr/8OS3JN02CakLS9RiyuQuWm8Fk7XybV7WyVzidNfKoZJ.hxFSi', 6),
+       ('gg', 'Маликов', '$2a$12$WIr/8OS3JN02CakLS9RiyuQuWm8Fk7XybV7WyVzidNfKoZJ.hxFSi', 7),
+       ('hh', 'Кузнецов', '$2a$12$WIr/8OS3JN02CakLS9RiyuQuWm8Fk7XybV7WyVzidNfKoZJ.hxFSi', 8),
+       ('zz', 'admin', '$2a$12$WIr/8OS3JN02CakLS9RiyuQuWm8Fk7XybV7WyVzidNfKoZJ.hxFSi', 9);
 
 DROP TABLE IF EXISTS USER_ROLE CASCADE;
 
@@ -44,4 +50,5 @@ create table USER_ROLE
 );
 
 insert into user_role (USER_ID, ROLE)
-VALUES (1, 'USER'), (2, 'USER'), (3, 'ADMIN')
+VALUES (1, 'USER'), (2, 'USER'), (3, 'USER'), (4, 'USER'), (5, 'USER'),
+       (6, 'USER'), (7, 'USER'), (8, 'USER'), (9, 'ADMIN');
--- src/main/resources/DB/8_populPosition_User.sql
+++ src/main/resources/DB/8_populPosition_User.sql
@@ -6,4 +6,4 @@ CREATE TABLE position_user(
 );
 
 INSERT INTO position_user(position_id, user_id)
-VALUES (1, 1), (2, 2), (9, 3);
+VALUES (1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9);
