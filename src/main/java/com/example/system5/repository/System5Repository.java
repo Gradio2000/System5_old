7fb72123720ddb2a09fd94a254b27a08fb815084
Gradio2000 <laskin999@gmail.com>
2022-05-06 22:24:56 +0500

    rebased

--- src/main/java/com/example/system5/repository/System5Repository.java
+++ src/main/java/com/example/system5/repository/System5Repository.java
@@ -14,9 +14,4 @@ public interface System5Repository extends JpaRepository<System5, Integer> {
     List<System5> findAllByUserId(Integer user_id);
 
     List<System5> findByUserId(int id);
-
-    @Query(nativeQuery = true,
-    value = "UPDATE system5 SET resempl1 = :resempl1 WHERE user_id = :user_id AND month = :month")
-    void updateSistem5 (String resempl1, String month);
-
 }
