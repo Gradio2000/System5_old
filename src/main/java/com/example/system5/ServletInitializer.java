23be63b123bc0a0e0d980c7318b5f0a728eefee4
Gradio2000 <laskin999@gmail.com>
2022-05-25 19:33:16 +0500

    рефактор запуска

--- src/main/java/com/example/system5/ServletInitializer.java
+++ src/main/java/com/example/system5/ServletInitializer.java
@@ -1,15 +1,20 @@
 package com.example.system5;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(System5Application.class);
+        return application.sources(ServletInitializer.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServletInitializer.class, args);
     }
 
 }
