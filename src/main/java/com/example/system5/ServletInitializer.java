1efc7edde0ba8679cf402cfff9de40100c126208
Gradio2000 <laskin999@gmail.com>
2022-05-30 12:51:50 +0500

    попытка деплоить не получается

--- src/main/java/com/example/system5/ServletInitializer.java
+++ src/main/java/com/example/system5/ServletInitializer.java
@@ -1,11 +1,11 @@
 package com.example.system5;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
