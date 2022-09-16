a436e3eb7177467053215cd40fd2b4e856a5bfcb
Gradio2000 <laskin999@gmail.com>
2022-09-16 18:57:49 +0500

    Version

--- src/main/java/com/example/system5/util/Version.java
+++ src/main/java/com/example/system5/util/Version.java
@@ -2,18 +2,24 @@ package com.example.system5.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
 
 public class Version {
     public static String getVersion(){
+        URL url = Version.class.getClassLoader().getResource("properties.properties");
         String version = null;
-        try (InputStream input = Files.newInputStream(Paths.get("src/main/resources/properties.properties"))) {
-            Properties prop = new Properties();
-            prop.load(input);
-            version = prop.getProperty("version");
-        } catch (IOException ex) {
+        try {
+            assert url != null;
+            try (InputStream input = Files.newInputStream(Paths.get(url.toURI()))) {
+                Properties prop = new Properties();
+                prop.load(input);
+                version = prop.getProperty("version");
+            }
+        } catch (IOException | URISyntaxException ex) {
             ex.printStackTrace();
         }
         return version;
